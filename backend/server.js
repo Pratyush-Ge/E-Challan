706119b5d32d9a80b03ceb97360bdb1bb3ce9d0f
Pratyush <pratyushghatole2003@gmail.com>
2024-05-05 00:58:51 +0530

    port

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(cors());
